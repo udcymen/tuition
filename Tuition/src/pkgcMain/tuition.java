9a4af7f4e80b4e9be2a5dc04df9d12b110d98a09
samue <samue@128.4.157.135>
2017-02-26 21:33:20 -0500

    Initial Commit

---
+++ Tuition/src/pkgcMain/tuition.java
@@ -0,0 +1,19 @@
+package pkgcMain;
+
+import java.util.Scanner;
+
+public class tuition {
+	public static void main(String[] args){
+		double tuition, percentage, total;
+		Scanner var = new Scanner(System.in);
+		System.out.println("Enter your yearly tuition : ");
+		tuition = var.nextDouble();
+		System.out.println("Enter the inflation : ");
+		percentage = var.nextDouble();
+		total = tuition;
+		for(int i = 0; i < 3; i++){
+			total += Math.pow(tuition, (1 + percentage));
+		}
+		System.out.println("The total amount of tuition is " + total);
+	}
+}
